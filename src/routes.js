f76e140b682b01fd14c3fc4a19fb6cdf9feea3e7
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-24 16:26:19 -0300

    create auth route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
 
+const authMiddleware = require('./app/middlewares/auth');
+
+const UserController = require('./app/controllers/UserController');
+const SessionController = require('./app/controllers/SessionController');
+
 const app = new Router();
 
-app.get('/', (req, res) => {
-  res.json({ ok: true });
-});
+app.post('/user', UserController.store);
+app.post('/session', SessionController.store);
+
+app.use(authMiddleware);
+app.put('/user', UserController.update);
 
 
 module.exports = app;
