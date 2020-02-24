1dd838cfcbfb44123a02986f999e20c591f7c694
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-24 16:26:47 -0300

    config database

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,11 @@
+require('dotenv/config');
+
 module.exports = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   dialect: 'mysql',
   define: {
     timestamps: true,
