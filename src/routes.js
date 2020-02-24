dc1b788aacbcad84fc59a87657055f04062caf80
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-24 20:46:44 -0300

    Create file routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,23 @@
 const { Router } = require('express');
+const multer = require('multer');
 
+const multerConfig = require('./config/multer');
 const authMiddleware = require('./app/middlewares/auth');
 
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
+const FileController = require('./app/controllers/FileController');
 
 const app = new Router();
+const upload = multer(multerConfig);
 
 app.post('/user', UserController.store);
 app.post('/session', SessionController.store);
 
 app.use(authMiddleware);
 app.put('/user', UserController.update);
+
+app.post('/files', upload.single('file'), FileController.store);
 
 
 module.exports = app;
