2bb64a82940b6a7d6137060087bc21bb42081cd5
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-24 16:27:16 -0300

    update user model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,39 @@
 const Sequelize = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 const { Model } = Sequelize;
 
 class User extends Model {
   static init(sequelize) {
     super.init({
+      email: Sequelize.STRING,
+      name: Sequelize.STRING,
+      sex: Sequelize.STRING,
+      male_interest: Sequelize.BOOLEAN,
+      female_interest: Sequelize.BOOLEAN,
+      birthday: Sequelize.DATE,
+      bio: Sequelize.STRING,
+      campus: Sequelize.STRING,
+      phone: Sequelize.STRING,
+      password: Sequelize.VIRTUAL,
+      password_hash: Sequelize.STRING,
+      admin: Sequelize.BOOLEAN,
 
     }, {
       sequelize,
     });
 
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
     return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 module.exports = User;
