88044c027987791c002f4ee1c0636f9247083846
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-24 20:46:18 -0300

    update database

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,9 @@ const Sequelize = require('sequelize');
 
 const dbConfig = require('../config/database');
 const User = require('../app/models/User');
+const File = require('../app/models/File');
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -12,7 +13,9 @@ class Database {
 
   init() {
     this.connection = new Sequelize(dbConfig);
-    models.map((model) => model.init(this.connection));
+    models
+      .map((model) => model.init(this.connection))
+      .map((model) => model.associate && model.associate(this.connection.models));
   }
 }
 
