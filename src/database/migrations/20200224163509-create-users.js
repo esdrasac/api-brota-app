9314fd13ae070a14b6b5c136a74821501ae6a5c6
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-24 16:28:02 -0300

    create user migration

--- src/database/migrations/20200224163509-create-users.js
+++ src/database/migrations/20200224163509-create-users.js
@@ -7,7 +7,60 @@ module.exports = {
       autoIncrement: true,
       primaryKey: true,
     },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    sex: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    male_interest: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    female_interest: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    birthday: {
+      type: Sequelize.DATE,
+    },
+    bio: {
+      type: Sequelize.STRING,
+    },
+    campus: {
+      type: Sequelize.STRING,
+    },
+    phone: {
+      type: Sequelize.STRING,
+    },
+    password_hash: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    admin: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
   }),
 
-  down: (queryInterface, Sequelize) => queryInterface.dropTable('users'),
+  down: (queryInterface) => queryInterface.dropTable('users'),
 };
