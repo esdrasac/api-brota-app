e470652c34bb787a0ebfd9773d53838736248751
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-24 16:29:35 -0300

    update index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config({ path: './.env' });
-
 const app = require('./app');
 
 const log = require('./config/logger');
+
+log.info(process.env.MYSQL_USER);
 
 app.listen(process.env.PORT, () => {
   log.info(`Application listening on PORT[${process.env.PORT}]`);
