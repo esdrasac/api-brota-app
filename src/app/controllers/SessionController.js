816831185052207dcc099747fadda7e149ec0f9f
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-24 16:28:47 -0300

    create SessionController

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,49 @@
+const yup = require('yup');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+const authConfig = require('../../config/auth');
+
+class SessionController {
+  async store(req, res) {
+    const schema = yup.object().shape({
+      email: yup.string()
+        .required()
+        .email(),
+      password: yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+module.exports = new SessionController();
