e3876747e7f6368ebedc10a66925109c791880fc
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-24 20:44:16 -0300

    Create user model

---
+++ src/app/models/File.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+const { Model } = Sequelize;
+
+class File extends Model {
+  static init(sequelize) {
+    super.init({
+      name: Sequelize.STRING,
+      path: Sequelize.STRING,
+    }, {
+      sequelize,
+    });
+
+    return this;
+  }
+}
+module.exports = File;
