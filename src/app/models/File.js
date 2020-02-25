1231fb660587b3111b654c17d80844f0bcc54d56
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-25 01:11:49 -0300

    update routes

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -5,7 +5,6 @@ const { Model } = Sequelize;
 class File extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
       path: Sequelize.STRING,
     }, {
       sequelize,
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ const multerConfig = require('./config/multer');
 const authMiddleware = require('./app/middlewares/auth');
 
 const UserController = require('./app/controllers/UserController');
+const LikeController = require('./app/controllers/LikeController');
+const DislikeController = require('./app/controllers/DislikeController');
 const SessionController = require('./app/controllers/SessionController');
 const FileController = require('./app/controllers/FileController');
 
@@ -15,7 +17,12 @@ app.post('/user', UserController.store);
 app.post('/session', SessionController.store);
 
 app.use(authMiddleware);
+
+app.get('/user', UserController.index);
 app.put('/user', UserController.update);
+
+app.post('/user/:targetId/likes', LikeController.store);
+app.post('/user/:targetId/dislikes', DislikeController.store);
 
 app.post('/files', upload.single('file'), FileController.store);
 
