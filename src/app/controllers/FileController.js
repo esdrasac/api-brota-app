2f6ad780a8927a3eb153ea373c6d0d0b1a33e780
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-25 01:12:53 -0300

    update FileController

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -1,13 +1,28 @@
+const log = require('../../config/logger');
+const UserSchema = require('../schemas/UserSchema');
 const File = require('../models/File');
 
 class FileController {
   async store(req, res) {
     const { originalname: name, filename: path } = req.file;
+    let file;
+    try {
+      file = await File.create({
+        name,
+        path,
+      });
 
-    const file = await File.create({
-      name,
-      path,
-    });
+      const userMongo = await UserSchema.findOne({ id: req.userId });
+      userMongo.avatar_id = {
+        name,
+        path,
+      };
+
+      await userMongo.save();
+    } catch (err) {
+      log.error(err);
+    }
+
 
     return res.json(file);
   }
