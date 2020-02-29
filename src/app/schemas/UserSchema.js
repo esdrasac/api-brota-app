577115d2c4d906bc386f2bc49fe18c0200ce55e3
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-28 21:53:31 -0300

    update User Schema

--- src/app/schemas/UserSchema.js
+++ src/app/schemas/UserSchema.js
@@ -3,31 +3,40 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   id: {
     type: Number,
+    default: null,
   },
   name: {
     type: String,
+    default: null,
   },
   bio: {
     type: String,
+    default: null,
   },
   sex: {
     type: String,
+    default: null,
   },
   male_interest: {
     type: Boolean,
+    default: false,
   },
   female_interest: {
     type: Boolean,
+    default: false,
   },
   email: {
     type: String,
+    default: null,
   },
   avatar_id: {
     name: {
       type: String,
+      default: null,
     },
     path: {
       type: String,
+      default: null,
     },
   },
   likes: [{
