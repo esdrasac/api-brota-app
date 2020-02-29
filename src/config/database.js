67b61a668294ab73bf918383240152693723ab57
Esdras Aguilar <esdras_ac2010@hotmail.com>
2020-02-28 21:54:05 -0300

    update database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 require('dotenv/config');
 
 module.exports = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  dialect: 'mysql',
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  dialect: 'postgres',
   define: {
     timestamps: true,
     underscored: true,
